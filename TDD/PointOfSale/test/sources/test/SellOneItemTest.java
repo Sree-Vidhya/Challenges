b3994c8a514185bcd04ebe727ac26960751f95f6
Sree <sree.vidhya@myob.com>
2019-09-25 10:14:31 +1000

    sale decides to display the price

--- TDD/PointOfSale/test/sources/test/SellOneItemTest.java
+++ TDD/PointOfSale/test/sources/test/SellOneItemTest.java
@@ -1,6 +1,9 @@
 package test;
 
+import org.junit.Ignore;
 import org.junit.Test;
+
+import javax.swing.*;
 
 import static org.junit.Assert.assertEquals;
 
@@ -9,19 +12,40 @@ public class SellOneItemTest
     @Test
     public void name() throws Exception {
         final Display display = new Display();
-        final Sale sale = new Sale();
+        final Sale sale = new Sale(String "12345");
         sale.onBarcode("12345");
         assertEquals("$7.95",display.getText());
     }
 
+    @Test
+    @Ignore("Refactoring...")
+    public void anotherProductFound() throws Exception {
+        final Display display = new Display();
+        final Sale sale = new Sale();
+        sale.onBarcode("23456");
+        assertEquals("$12.50",display.getText());
+    }
+
     public static class Display {
+        private String text;
         public String getText() {
-            return "$7.95";
+            return text;
+        }
+
+        public void setText(String text) {
+            this.text = text;
         }
     }
 
     public static class Sale {
+        public Sale(Display display) {
+            this.display = display;
+        }
+
+        private Display display;
+
         public void onBarcode(String barcode) {
+            display.setText("$7.95");
         }
     }
 }
