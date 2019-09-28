83f6afb046a5be6f904e3e28ef3efa931903fb17
Sree <sree.vidhya@myob.com>
2019-09-28 23:24:49 +1000

    Removed duplicate tet setup.

--- TDD/PointOfSale/test/sources/test/SellOneItemTest.java
+++ TDD/PointOfSale/test/sources/test/SellOneItemTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Collections;
@@ -10,37 +11,35 @@ import static org.junit.Assert.assertEquals;
 
 public class SellOneItemTest
 {
-    @Test
-    public void productFound() throws Exception
-    {
-        final Display display = new Display();
-        final Sale sale = new Sale(display,new HashMap<String, String>() {{
+
+    private Display display;
+    private Sale sale;
+
+    @Before
+    public void setUp() throws Exception {
+        display = new Display();
+        sale = new Sale(display,new HashMap<String, String>() {{
             put("12345", "$7.95");
             put("23456", "$12.50");
         }});
+    }
+
+    @Test
+    public void productFound() throws Exception
+    {
         sale.onBarcode("12345");
-        assertEquals("$7.95",display.getText());
+        assertEquals("$7.95", display.getText());
     }
 
     @Test
     public void anotherProductFound() throws Exception
     {
-        final Display display = new Display();
-        final Sale sale = new Sale(display,new HashMap<String, String>() {{
-            put("12345", "$7.95");
-            put("23456", "$12.50");
-        }});
         sale.onBarcode("23456");
         assertEquals("$12.50",display.getText());
     }
     @Test
     public void productNotFound() throws Exception
     {
-        final Display display = new Display();
-        final Sale sale = new Sale(display,new HashMap<String, String>() {{
-            put("12345", "$7.95");
-            put("23456", "$12.50");
-        }});
         sale.onBarcode("99999");
         assertEquals("product Not Found for 99999", display.getText());
     }
@@ -48,7 +47,6 @@ public class SellOneItemTest
     @Test
     public void emptyBarcode() throws Exception
     {
-        final Display display = new Display();
         final Sale sale = new Sale(display, null);
         sale.onBarcode("");
         assertEquals("scanning error:empty barcode",display.getText());
